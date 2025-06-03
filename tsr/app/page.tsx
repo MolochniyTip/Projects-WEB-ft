97a83b5cf9ddb50267c2cdf4356664f188a8d1c1
MolochniyTip <dyadyasasha280404@gmail.com>
2025-06-03 14:22:21 +0500

    remove

--- tsr/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-      <div>
-        <h1 className="text-2xl font-bold">AI Translator</h1>
-      </div>
-  )
-}
--- tsr/app/voice/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Voice() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">AI Talker</h1>
-        </div>
-    )
-}
